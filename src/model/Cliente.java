5cbf6745f1d7cd587da994e7c14d84e0897798e2
BrenoStadnik <101637478+BrenoStadnik@users.noreply.github.com>
2022-04-08 12:25:55 -0300

    Commit

---
+++ src/model/Cliente.java
@@ -0,0 +1,49 @@
+package model;
+
+public class Cliente {
+
+    private String nome;
+    private int telefone;
+    private String email;
+    private int cpf;
+
+
+    public Cliente(String nome, int telefone, String email, int cpf) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+}
